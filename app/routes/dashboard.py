5e80da05ec9508e643cc1ab29b4682c0722a03d3
beamchristian <ckbeamcoding@gmail.com>
2022-08-01 19:50:24 -0400

    inital commit with basic file structure and basic get routes

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -5,7 +5,7 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 @bp.route('/')
 def dash():
   return render_template('dashboard.html')
-  
+
 @bp.route('/edit/<id>')
 def edit(id):
   return render_template('edit-post.html')
